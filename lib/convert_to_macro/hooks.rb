7191bfeba15c03e55ef2659d6a9a27ff59a1d619
tom-mats <dawn.gray.sky@gmail.com>
2016-02-24 06:55:33 +0900

    fix regex

--- lib/convert_to_macro/hooks.rb
+++ lib/convert_to_macro/hooks.rb
@@ -4,12 +4,12 @@ module ConvertToMacro
       '(f?pr\d+\w*)' => '\1{{fixfile(\2)}}\3',
       'vi?e?w:((?:\w|\.)+)' => '\1{{fixfile(\2)}}\2',
       '(\w\w\w?\w?-\d\d\d)' => '\1{{alarm(' + "\\2".upcase + ')}}\3',
-      '#(?:RB|rb|US|us)(\d+)' => '{{usersupport(\1)}}',
+      '#(?:RB|rb|US|us)(\d+)' => '\1{{usersupport(\2)}}\3',
       '(V\d\.\d\d(?:\d+|P\/(?:\d|\w)+))' => '\1[[test:\2]]\3'
       }
     def parse(data)
       REGEXP_LIST.each do |k, v|
-        data.gsub!(/(\A|[^\(])#{k}(\Z|[^\)])/, v)
+        data.gsub!(/(\A|[^\(:f])#{k}(\Z|[^\)])/, v)
       end
     end
     def controller_issues_new_before_save(context={})

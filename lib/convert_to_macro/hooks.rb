bd1d0891269aceb01cf69ce63e72ad6d1e3c3834
tom-mats <dawn.gray.sky@gmail.com>
2016-02-23 23:22:45 +0900

    fix regex

--- lib/convert_to_macro/hooks.rb
+++ lib/convert_to_macro/hooks.rb
@@ -1,23 +1,23 @@
 module ConvertToMacro
   class Hooks < Redmine::Hook::Listener
     REGEXP_LIST = {
-      '(\A|\s|,)(f?pr\d+\w*)(\s|,|\Z)' => '\1{{fixfile(\2)}}\3',
-      'vi?e?w:((?:\w|\.)+)' => '{{fixfile(\1)}}',
-      '(\A|[^\(])(\w\w\w?\w?-\d\d\d)(\Z|[^\)])' => '\1{{alarm(' + "\\2".upcase + ')}}\3',
+      '(f?pr\d+\w*)' => '\1{{fixfile(\2)}}\3',
+      'vi?e?w:((?:\w|\.)+)' => '\1{{fixfile(\2)}}\2',
+      '(\w\w\w?\w?-\d\d\d)' => '\1{{alarm(' + "\\2".upcase + ')}}\3',
       '#(?:RB|rb|US|us)(\d+)' => '{{usersupport(\1)}}',
-      '(\A|[^\(])(V\d\.\d\d(?:\d+|P\/(?:\d|\w)+))(\Z|[^\)])' => '\1[[test:\2]]\3'
+      '(V\d\.\d\d(?:\d+|P\/(?:\d|\w)+))' => '\1[[test:\2]]\3'
       }
     def controller_issues_new_before_save(context={})
       issue = context[:issue]
       REGEXP_LIST.each do |k, v|
-        issue.description.gsub!(/#{k}/, v)
+        issue.description.gsub!(/(\A|[^\(])#{k}(\Z|[^\)])/, v)
       end
     end
     def controller_issues_edit_before_save(context={})
       issue = context[:issue]
       REGEXP_LIST.each do |k, v|
-        issue.description.gsub!(/#{k}/, v)
-        issue.notes.gsub!(/#{k}/, v)
+        issue.description.gsub!(/(\A|[^\(])#{k}(\Z|[^\)])/, v)
+        issue.notes.gsub!(/(\A|[^\(])#{k}(\Z|[^\)])/, v)
       end
     end
   end

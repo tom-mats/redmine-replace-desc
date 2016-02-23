10ae48fef1fce89d2b8ceac2bb5ca4e37e560229
tom-mats <dawn.gray.sky@gmail.com>
2016-02-23 22:32:10 +0900

    fix

--- lib/convert_to_macro/hooks.rb
+++ lib/convert_to_macro/hooks.rb
@@ -4,13 +4,14 @@ module ConvertToMacro
       "(\s|,)(f?pr\d+\w*)(\s|,)" => '\1{{fixfile(\2)}}\3',
       "vi?e?w:([A-Za-z0-9\._]+)" => '{{fixfile(\1)}}'
       }
-    def controller_issue_new_before_save(context={})
+    def controller_issues_new_before_save(context={})
       issue = context[:issue]
+      p issue.description
       REGEXP_LIST.each do |k, v|
         issue.description.gsub!(/#{k}/, v)
       end
     end
-    def controller_issue_edit_before_save(context={})
+    def controller_issues_edit_before_save(context={})
     end
   end
 end
